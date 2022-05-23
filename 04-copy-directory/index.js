fb017d9bb8fd4ec87879942af65f5d36329ece7c
Anna <annank18@gmail.com>
2022-05-24 00:50:50 +0400

    feat: done 04-copy-directory (Tue,May 24, 2022 12:50:50 AM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const path = require ('path');
+const fs = require('fs');
+const { resolve } = require ('path');
+const { copyFile } = require ('fs');
+
+const files = path.resolve (__dirname, 'files');
+const filesCopy = path.resolve (__dirname, 'files-copy');
+
+async function copyDir(files, filesCopy) {
+    const sourceStat = await fs.promises.stat(files);
+    if (!sourceStat.isFile()) {
+        await fs.promises.mkdir(filesCopy);
+        for (item of await fs.promises.readdir(files)) {
+        copyDir(path.resolve(files, item), path.resolve(filesCopy, item));
+        }
+    } else {
+        await fs.promises.copyFile(files, filesCopy);
+    }
+}
+
+async function updateFolder(files, filesCopy) {
+    await fs.promises.rm(filesCopy, { recursive: true, force: true });
+    copyDir(files, filesCopy);
+}
+
+updateFolder (files, filesCopy);
 
